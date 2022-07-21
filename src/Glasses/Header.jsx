a688dd721b069b78075217613893027998253277
Victor Robert <rinmai2019@gmail.com>
2022-07-21 10:53:52 +0700

    up up up

--- src/Glasses/Header.jsx
+++ src/Glasses/Header.jsx
@@ -2,10 +2,7 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div
-      style={{ letterSpacing: "1px", padding: "30px 0" }}
-      className="bg-dark text-center fs-3 text-white opacity-75"
-    >
+    <div className="bg-dark text-center fs-3 text-white opacity-75 py-4">
       TRY GLASSES APP ONLINE
     </div>
   );
